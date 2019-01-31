9983045cf7ab31cc2626d15aa4b2b18e8b68e6d6
Alexandre Freire <xandrefreire@gmail.com>
2019-01-31 10:43:22 +0000

    Add RateListViewController and RateCell

---
+++ CurrencyRates/Core/RateList/Presentation/RateCell.swift
@@ -0,0 +1,22 @@
+//
+//  RateCell.swift
+//  Core
+//
+//  Created by Alexandre Freire on 31/01/2019.
+//  Copyright © 2019 Alexandre Freire. All rights reserved.
+//
+
+import UIKit
+
+class RateCell: UITableViewCell {
+
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var subtitleLabel: UILabel!
+    @IBOutlet weak var textField: UITextField!
+    
+    override func prepareForReuse() {
+        titleLabel.text = nil
+        subtitleLabel.text = nil
+        textField.text = nil
+    }
+}
---
+++ CurrencyRates/Core/RateList/Presentation/RateListViewController.swift
@@ -0,0 +1,17 @@
+//
+//  RateListViewController.swift
+//  Core
+//
+//  Created by Alexandre Freire on 31/01/2019.
+//  Copyright © 2019 Alexandre Freire. All rights reserved.
+//
+
+import UIKit
+
+class RateListViewController: UIViewController {
+
+    @IBOutlet weak var tableView: UITableView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
